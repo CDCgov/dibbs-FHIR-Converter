24d19d648017bfd8d49b0b05e1ff654adbfe7107
Austin Hall <austin.hall@skylight.digital>
2025-07-10 17:26:52 -0400

    finish adding rr section to eicr

--- src/FHIRConverterAPI/Program.cs
+++ src/FHIRConverterAPI/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Xml;
 using System.Xml.Linq;
 using System.Xml.XPath;
 
@@ -21,7 +22,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
 app.MapGet("/", () => new { status = "OK" })
 .WithName("HealthCheck")
@@ -130,8 +131,7 @@ string AddRRDataToEicr(string inputData, string rrData)
     // TODO: Will the schema instance namespace ever actually be missing?
     var ecrLines = inputData.Split(
         ["\n", "\r\n"],
-        StringSplitOptions.None
-    ).ToList();
+        StringSplitOptions.None).ToList();
     var startIndex = ecrLines.FindIndex(line => line.TrimStart().StartsWith("<ClinicalDocument"));
     var endIndex = ecrLines.FindIndex(startIndex, line => line.TrimEnd().EndsWith('>'));
 
@@ -147,7 +147,7 @@ string AddRRDataToEicr(string inputData, string rrData)
     try
     {
         var ecrXDocument = XDocument.Parse(inputData);
-        var defaultNamespace = ecrXDocument.Root?.GetDefaultNamespace().NamespaceName;
+        var rrXDocument = XDocument.Parse(rrData);
 
         // Check for eICR Processing Status entry (required & only available in RR)
         if (ecrXDocument.XPathSelectElement("//*[@root=\"2.16.840.1.113883.10.20.15.2.3.29\"]") is not null)
@@ -158,117 +158,83 @@ string AddRRDataToEicr(string inputData, string rrData)
 
         // If eICR >=R3, remove (optional) RR section that came from eICR
         // This is duplicate/incomplete info from RR
-        var ecrVersion = ecrXDocument.XPathEvaluate("string(//*[@root=\"2.16.840.1.113883.10.20.15.2\"]/@extension)").ToString();
-        Console.WriteLine(ecrVersion);
-        // if (ecrVersion >= "2021-01-01")
-        // {
-        //     namespaces = {"hl7": "urn:hl7-org:v3"}
-        //     rr_from_eicr_arr = ecr.xpath(
-        //         '//hl7:component[hl7:section/hl7:templateId[@root="2.16.840.1.113883.10.20.15.2.2.5" and @extension="2021-01-01"]]',
-        //         namespaces=namespaces,
-        //     )
-        // }
+        var ecrVersion = ecrXDocument.XPathEvaluate("string(//*[@root=\"2.16.840.1.113883.10.20.15.2\"]/@extension)");
+        if (ecrVersion is not null && DateTime.Parse(ecrVersion.ToString()) >= DateTime.Parse("2021-01-01"))
+        {
+            var names = new XmlNamespaceManager(ecrXDocument.CreateNavigator().NameTable);
+            names.AddNamespace("hl7", "urn:hl7-org:v3");
+            var rrFromEicr = ecrXDocument.XPathSelectElement(
+                "//hl7:component[hl7:section/hl7:templateId[@root=\"2.16.840.1.113883.10.20.15.2.2.5\"]]",
+                names);
 
-        //     if rr_from_eicr_arr:
-        //         rr_from_eicr = rr_from_eicr_arr[0]
-        //         rr_parent = rr_from_eicr.getparent()
-        //         if rr_parent is not None:
-        //             rr_parent.remove(rr_from_eicr)
+            rrFromEicr?.Remove();
+        }
 
-        // # Create the tags for elements we'll be looking for
-        // rr_tags = [
-        //     "templateId",
-        //     "id",
-        //     "code",
-        //     "title",
-        //     "effectiveTime",
-        //     "confidentialityCode",
-        // ]
-        // rr_tags = ["{urn:hl7-org:v3}" + tag for tag in rr_tags]
-        // rr_elements = []
+        // Create the tags for elements we'll be looking for
+        var rrTags = new string[]
+        {
+            "templateId",
+            "id",
+            "code",
+            "title",
+            "effectiveTime",
+            "confidentialityCode",
+        };
 
-        // # Find root-level elements and add them to a list
-        // for tag in rr_tags:
-        //     rr_elements.append(rr.find(f"./{tag}", namespaces=rr.nsmap))
+        var rrElements = new List<XElement>();
+        var rrNames = new XmlNamespaceManager(rrXDocument.CreateNavigator().NameTable);
+        rrNames.AddNamespace("hl7", "urn:hl7-org:v3");
 
-        // # Find the nested entry element that we need
-        // entry_tag = "{urn:hl7-org:v3}" + "component/structuredBody/component/section/entry"
-        // rr_nested_entries = rr.findall(f"./{entry_tag}", namespaces=rr.nsmap)
+        // Find root-level elements and add them to a list
+        foreach (var tag in rrTags)
+        {
+            var element = rrXDocument.XPathSelectElement($"/*/hl7:{tag}", rrNames);
+            if (element is not null)
+            {
+                rrElements.Add(element);
+            }
+        }
 
-        // organizer_tag = "{urn:hl7-org:v3}" + "organizer"
+        // Find the nested entry element that we need
+        var rrEntry = rrXDocument.XPathSelectElement(
+            $"/*/hl7:component/hl7:structuredBody/hl7:component/hl7:section/hl7:entry" +
+            "[@typeCode=\"DRIV\"]/hl7:organizer[@classCode=\"CLUSTER\" and @moodCode=\"EVN\"]/..", rrNames);
 
-        // # For now we assume there is only one matching entry
-        // rr_entry = None
-        // for entry in rr_nested_entries:
-        //     if entry.attrib and "DRIV" in entry.attrib["typeCode"]:
-        //         organizer = entry.find(f"./{organizer_tag}", namespaces=entry.nsmap)
-        //         if (
-        //             organizer is not None
-        //             and "CLUSTER" in organizer.attrib["classCode"]
-        //             and "EVN" in organizer.attrib["moodCode"]
-        //         ):
-        //             rr_entry = entry
-        //             exit
+        // find the status in the RR utilizing the templateid root
+        // codes specified from the APHL/LAC Spec
+        var rrEntryForStatusCodes = rrXDocument.XPathSelectElements(
+            "/*/hl7:component/hl7:structuredBody/hl7:component/hl7:section/hl7:templateId" +
+            "[@root=\"2.16.840.1.113883.10.20.15.2.2.3\"]/../hl7:entry/hl7:act/hl7:templateId[@root=\"2.16.840.1.113883.10.20.15.2.3.29\"]/../..",
+            rrNames);
 
-        // # find the status in the RR utilizing the templateid root
-        // # codes specified from the APHL/LAC Spec
-        // base_tag_for_status = (
-        //     "{urn:hl7-org:v3}" + "component/structuredBody/component/section"
-        // )
-        // template_id_tag = "{urn:hl7-org:v3}" + "templateId"
-        // entry_status_tag = "{urn:hl7-org:v3}" + "entry"
-        // act_status_tag = "{urn:hl7-org:v3}" + "act"
-        // sections_for_status = rr.findall(f"./{base_tag_for_status}", namespaces=rr.nsmap)
-        // rr_entry_for_status_codes = None
-        // for status_section in sections_for_status:
-        //     template_id = status_section.find(
-        //         f"./{template_id_tag}", namespaces=status_section.nsmap
-        //     )
-        //     if (
-        //         template_id is not None
-        //         and "2.16.840.1.113883.10.20.15.2.2.3" in template_id.attrib["root"]
-        //     ):
-        //         for entry in status_section.findall(
-        //             f"./{entry_status_tag}", namespaces=status_section.nsmap
-        //         ):
-        //             for act in entry.findall(f"./{act_status_tag}", namespaces=entry.nsmap):
-        //                 entry_act_template_id = act.find(
-        //                     f"./{template_id_tag}", namespaces=act.nsmap
-        //                 )
-        //                 if (
-        //                     entry_act_template_id is not None
-        //                     and "2.16.840.1.113883.10.20.15.2.3.29"
-        //                     in entry_act_template_id.attrib["root"]
-        //                 ):
-        //                     # only anticipating one status code
-        //                     rr_entry_for_status_codes = entry
-        //                     exit
+        // Create the section element with root-level elements
+        // and entry to insert in the eICR
+        if (rrEntry is not null)
+        {
+            var ecrSection = new XElement("section");
+            foreach (var element in rrElements)
+            {
+                ecrSection.Add(element);
+            }
 
-        // # Create the section element with root-level elements
-        // # and entry to insert in the eICR
-        // ecr_section = None
-        // if rr_entry is not None:
-        //     ecr_section_tag = "{urn:hl7-org:v3}" + "section"
-        //     ecr_section = etree.Element(ecr_section_tag)
-        //     ecr_section.extend(rr_elements)
-        //     if rr_entry_for_status_codes is not None:
-        //         ecr_section.append(rr_entry_for_status_codes)
-        //     ecr_section.append(rr_entry)
+            if (rrEntryForStatusCodes is not null)
+            {
+                ecrSection.Add(rrEntryForStatusCodes);
+            }
 
-        //     # Append the ecr section into the eCR - puts it at the end
-        //     ecr.append(ecr_section)
+            ecrSection.Add(rrEntry);
 
-        // ecr = etree.tostring(ecr, encoding="unicode", method="xml")
+            // Append the ecr section into the eCR - puts it at the end
+            ecrXDocument.Root.Add(ecrSection);
+        }
 
-        // return ecr
+        return ecrXDocument.ToString();
     }
     catch (Exception ex)
     {
         Console.WriteLine($"Error processing eICR document: {ex.Message}");
         throw new Exception($"Error processing eICR document: {ex.Message}");
     }
-
-    return inputData;
 }
 
 public partial class Program

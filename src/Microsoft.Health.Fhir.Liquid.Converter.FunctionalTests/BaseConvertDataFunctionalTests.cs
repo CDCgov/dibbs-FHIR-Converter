ef8123e285569cf252e1ad5283b49cfad7b7343f
Mary McGrath <m.c.mcgrath13@gmail.com>
2025-07-24 11:31:25 -0400

    fix: comments, more things are already failing 😭

--- src/Microsoft.Health.Fhir.Liquid.Converter.FunctionalTests/BaseConvertDataFunctionalTests.cs
+++ src/Microsoft.Health.Fhir.Liquid.Converter.FunctionalTests/BaseConvertDataFunctionalTests.cs
@@ -230,9 +230,17 @@ namespace Microsoft.Health.Fhir.Liquid.Converter.FunctionalTests
         {
             var data = new List<string[]>
             {
+                // Array has the following fields:
+                // [
+                //   1. Root template, 
+                //   2. ecr file, 
+                //   3. expected fhir file, 
+                //   4. whether the file should fail at parsing or validation when testing if valid (if it is fully valid, "validation" is what should be there)
+                //   5. The number of expected failures at the step in (4)
+                // ]
                 new[] { @"EICR", @"eCR_full.xml", @"eCR_full-expected.json", "validation", "19" },
                 new[] { @"EICR", @"eCR_RR_combined_3_1.xml", @"eCR_RR_combined_3_1-expected.json", "parsing", "3" },
-                new[] { @"EICR", @"eCR_EveEverywoman.xml", @"eCR_EveEverywoman-expected.json", "parsing", "16" },
+                new[] { @"EICR", @"eCR_EveEverywoman.xml", @"eCR_EveEverywoman-expected.json", "parsing", "19" },
             };
             return data.Select(item => new[]
             {

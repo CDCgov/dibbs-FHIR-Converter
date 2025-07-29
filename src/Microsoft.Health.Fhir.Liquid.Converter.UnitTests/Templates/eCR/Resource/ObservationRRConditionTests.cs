b395f6cdc0668ad8f1c85a347216f46666fd9987
angelathe <angela.the@skylight.digital>
2025-07-29 14:00:09 -0400

    fix unit test: Observation RR condition

--- src/Microsoft.Health.Fhir.Liquid.Converter.UnitTests/Templates/eCR/Resource/ObservationRRConditionTests.cs
+++ src/Microsoft.Health.Fhir.Liquid.Converter.UnitTests/Templates/eCR/Resource/ObservationRRConditionTests.cs
@@ -7,9 +7,6 @@ using Xunit;
 using Microsoft.Health.Fhir.Liquid.Converter.Parsers;
 using System;
 
-// using Hl7.Fhir.Model;
-// using Hl7.Fhir.Serialization;
-
 namespace Microsoft.Health.Fhir.Liquid.Converter.UnitTests
 {
     public class ObservationRRConditionTests : BaseECRLiquidTests
@@ -44,149 +41,6 @@ namespace Microsoft.Health.Fhir.Liquid.Converter.UnitTests
                 <value code=""3928002"" codeSystem=""2.16.840.1.113883.6.96""
                     codeSystemName=""SNOMED CT""
                     displayName=""Zika virus disease (disorder)"" xsi:type=""CD"" />
-                <entryRelationship typeCode=""COMP"">
-                    <organizer classCode=""CLUSTER"" moodCode=""EVN"">
-                        <templateId extension=""2017-04-01""
-                            root=""2.16.840.1.113883.10.20.15.2.3.13"" />
-                        <id root=""fcf92143-4289-450e-9550-8d574facf626"" />
-                        <code code=""RRVS7""
-                            codeSystem=""2.16.840.1.114222.4.5.274""
-                            codeSystemName=""PHIN VS (CDC Local Coding System)""
-                            displayName=""Both patient home address and provider facility address""></code>
-                        <statusCode code=""completed"" />
-                        <participant typeCode=""LOC"">
-                            <templateId extension=""2017-04-01""
-                                root=""2.16.840.1.113883.10.20.15.2.4.2"" />
-                            <participantRole>
-                                <id extension=""12341234""
-                                    root=""2.16.840.1.113883.4.6"" />
-                                <code code=""RR8""
-                                    codeSystem=""2.16.840.1.114222.4.5.232""
-                                    codeSystemName=""PHIN Questions""
-                                    displayName=""Responsible Agency""></code>
-                                <addr use=""WP"">
-                                    <streetAddressLine>7777 Health Authority
-                                        Drive</streetAddressLine>
-                                    <city />
-                                    <state>State</state>
-                                    <postalCode>99999</postalCode>
-                                </addr>
-                                <telecom use=""WP"" value=""tel:+1-555-555-3555"" />
-                                <telecom use=""WP"" value=""fax:+1-955-555-3555"" />
-                                <telecom use=""WP""
-                                    value=""mailto:mail@healthauthoritywest.gov"" />
-                                <telecom use=""WP""
-                                    value=""https://www.healthauthoritywest.gov"" />
-                                <playingEntity>
-                                    <name>State Department of Health</name>
-                                    <desc>Responsible Agency Description</desc>
-                                </playingEntity>
-                            </participantRole>
-                        </participant>
-                        <participant typeCode=""LOC"">
-                            <templateId extension=""2017-04-01""
-                                root=""2.16.840.1.113883.10.20.15.2.4.3"" />
-                            <participantRole>
-                                <id extension=""12341234""
-                                    root=""2.16.840.1.113883.4.6"" />
-                                <code code=""RR12""
-                                    codeSystem=""2.16.840.1.114222.4.5.232""
-                                    codeSystemName=""PHIN Questions""
-                                    displayName=""Rules Authoring Agency"" />
-                                <addr use=""WP"">
-                                    <streetAddressLine>7777 Health Authority
-                                        Drive</streetAddressLine>
-                                    <city>City</city>
-                                    <state>State</state>
-                                    <postalCode>99999</postalCode>
-                                </addr>
-                                <telecom use=""WP"" value=""tel:+1-555-555-3555"" />
-                                <telecom use=""WP"" value=""fax:+1-955-555-3555"" />
-                                <telecom use=""WP""
-                                    value=""mailto:mail@healthauthoritywest.gov"" />
-                                <telecom use=""WP""
-                                    value=""https://www.healthauthoritywest.gov"" />
-                                <playingEntity>
-                                    <name>State Department of Health</name>
-                                    <desc>Rules Authoring Agency Description</desc>
-                                </playingEntity>
-                            </participantRole>
-                        </participant>
-                        <participant typeCode=""LOC"">
-                            <templateId extension=""2017-04-01""
-                                root=""2.16.840.1.113883.10.20.15.2.4.1"" />
-                            <participantRole>
-                                <id extension=""43214321""
-                                    root=""2.16.840.1.113883.4.6"" />
-                                <code code=""RR7""
-                                    codeSystem=""2.16.840.1.114222.4.5.232""
-                                    codeSystemName=""PHIN Questions""
-                                    displayName=""Routing Entity"" />
-                                <addr use=""WP"">
-                                    <streetAddressLine>7777 Health Authority
-                                        Drive</streetAddressLine>
-                                    <city>City</city>
-                                    <state>State</state>
-                                    <postalCode>99999</postalCode>
-                                </addr>
-                                <telecom use=""WP"" value=""tel:+1-555-555-3555"" />
-                                <telecom use=""WP"" value=""fax:+1-955-555-3555"" />
-                                <telecom use=""WP""
-                                    value=""mailto:mail@healthauthoritywest.gov"" />
-                                <telecom use=""WP""
-                                    value=""https://www.healthauthoritywest.gov"" />
-                                <playingEntity>
-                                    <name>State Department of Health Routing
-                                        Agency</name>
-                                    <desc>Routing Agency Description</desc>
-                                </playingEntity>
-                            </participantRole>
-                        </participant>
-                        <component typeCode=""COMP"">
-                            <observation classCode=""OBS"" moodCode=""EVN"">
-                                <templateId extension=""2017-04-01""
-                                    root=""2.16.840.1.113883.10.20.15.2.3.19"" />
-                                <id root=""e39d6ae2-8c6e-4638-9b33-412996586f41"" />
-                                <code code=""RR1""
-                                    codeSystem=""2.16.840.1.114222.4.5.232""
-                                    codeSystemName=""PHIN Questions""
-                                    displayName=""Determination of reportability"" />
-                                <value code=""RRVS1""
-                                    codeSystem=""2.16.840.1.114222.4.5.274""
-                                    codeSystemName=""PHIN VS (CDC Local Coding System)""
-                                    displayName=""Reportable"" xsi:type=""CD"" />
-                                <entryRelationship typeCode=""RSON"">
-                                    <observation classCode=""OBS"" moodCode=""EVN"">
-                                        <templateId extension=""2017-04-01""
-                                            root=""2.16.840.1.113883.10.20.15.2.3.26"" />
-                                        <id
-                                            root=""8709a342-56ad-425a-b7b1-76a16c2dd2d5"" />
-                                        <code code=""RR2""
-                                            codeSystem=""2.16.840.1.114222.4.5.232""
-                                            codeSystemName=""PHIN Questions""
-                                            displayName=""Determination of reportability reason"" />
-                                        <value xsi:type=""ST"">Reason for
-                                            determination of reportability</value>
-                                    </observation>
-                                </entryRelationship>
-                                <entryRelationship typeCode=""RSON"">
-                                    <observation classCode=""OBS"" moodCode=""EVN"">
-                                        <templateId extension=""2017-04-01""
-                                            root=""2.16.840.1.113883.10.20.15.2.3.27"" />
-                                        <id
-                                            root=""f2dfdffb-bccb-4ee4-9b6c-0ae82b15ada6"" />
-                                        <code code=""RR3""
-                                            codeSystem=""2.16.840.1.114222.4.5.232""
-                                            codeSystemName=""PHIN Questions""
-                                            displayName=""Determination of reportability rule"" />
-                                        <value xsi:type=""ST"">Rule used in
-                                            reportability determination</value>
-                                    </observation>
-                                </entryRelationship>
-                            </observation>
-                        </component>
-                    </organizer>
-                </entryRelationship>
             </observation>
             ";
 
@@ -212,12 +66,6 @@ namespace Microsoft.Health.Fhir.Liquid.Converter.UnitTests
             Assert.Equal("3928002", value.Coding.First().Code);
             Assert.Equal("http://snomed.info/sct", value.Coding.First().System);
             Assert.Equal("Zika virus disease (disorder)", value.Coding.First().Display);
-
-            Assert.Equal("Reportable", actualFhir.GetExtensionValue<CodeableConcept>("http://hl7.org/fhir/us/ecr/StructureDefinition/us-ph-determination-of-reportability-extension").Coding.First().Display);
-            var extRRReason = actualFhir.GetExtension("http://hl7.org/fhir/us/ecr/StructureDefinition/us-ph-determination-of-reportability-reason-extension");
-            Assert.Equal("Reason for determination of reportability", ((FhirString)extRRReason.Value).Value);
-            var extRRRule = actualFhir.GetExtension("http://hl7.org/fhir/us/ecr/StructureDefinition/us-ph-determination-of-reportability-rule-extension");
-            Assert.Equal("Rule used in reportability determination", ((FhirString)extRRRule.Value).Value);
         }
     }
 }

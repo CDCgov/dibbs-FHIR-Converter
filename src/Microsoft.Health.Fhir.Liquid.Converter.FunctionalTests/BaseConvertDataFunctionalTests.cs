993c565db89dd5e0952e5da62fa6790c07ef5010
Austin Hall <austin.hall@skylight.digital>
2025-10-08 14:18:56 -0400

    update expected validation errors

--- src/Microsoft.Health.Fhir.Liquid.Converter.FunctionalTests/BaseConvertDataFunctionalTests.cs
+++ src/Microsoft.Health.Fhir.Liquid.Converter.FunctionalTests/BaseConvertDataFunctionalTests.cs
@@ -240,9 +240,9 @@ namespace Microsoft.Health.Fhir.Liquid.Converter.FunctionalTests
                 //   5. The number of expected failures at the step in (4) for the numFailures parameter
                 // ]
                 new[] { @"EICR", @"eCR_full.xml", @"eCR_full-expected.json", "validation", "4" },
-                new[] { @"EICR", @"eCR_RR_combined_3_1.xml", @"eCR_RR_combined_3_1-expected.json", "validation", "10" },
-                new[] { @"EICR", @"eCR_EveEverywoman.xml", @"eCR_EveEverywoman-expected.json", "validation", "32" },
-                new[] { @"EICR", @"eicr04152020.xml", @"eicr04152020-expected.json", "validation", "15" },
+                new[] { @"EICR", @"eCR_RR_combined_3_1.xml", @"eCR_RR_combined_3_1-expected.json", "validation", "8" },
+                new[] { @"EICR", @"eCR_EveEverywoman.xml", @"eCR_EveEverywoman-expected.json", "validation", "29" },
+                new[] { @"EICR", @"eicr04152020.xml", @"eicr04152020-expected.json", "validation", "13" },
                 new[] { @"EICR", @"CDAR2_IG_PHCASERPT_R2_D2_SAMPLE.xml", @"CDAR2_IG_PHCASERPT_R2_D2_SAMPLE-expected.json", "validation", "15" },
             };
             return data.Select(item => new[]

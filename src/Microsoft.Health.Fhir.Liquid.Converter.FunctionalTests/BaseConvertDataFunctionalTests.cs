c5ad13addce49420681d5e04be03665b1e78f93c
Angela The <angela.the@skylight.digital>
2025-10-08 09:33:24 -0700

    Update num validation errors

--- src/Microsoft.Health.Fhir.Liquid.Converter.FunctionalTests/BaseConvertDataFunctionalTests.cs
+++ src/Microsoft.Health.Fhir.Liquid.Converter.FunctionalTests/BaseConvertDataFunctionalTests.cs
@@ -239,7 +239,7 @@ namespace Microsoft.Health.Fhir.Liquid.Converter.FunctionalTests
                 //   4. whether the file should fail at parsing or validation when testing if valid (if it is fully valid, "validation" is what should be there)
                 //   5. The number of expected failures at the step in (4) for the numFailures parameter
                 // ]
-                new[] { @"EICR", @"eCR_full.xml", @"eCR_full-expected.json", "validation", "6" },
+                new[] { @"EICR", @"eCR_full.xml", @"eCR_full-expected.json", "validation", "4" },
                 new[] { @"EICR", @"eCR_RR_combined_3_1.xml", @"eCR_RR_combined_3_1-expected.json", "validation", "10" },
                 new[] { @"EICR", @"eCR_EveEverywoman.xml", @"eCR_EveEverywoman-expected.json", "validation", "32" },
                 new[] { @"EICR", @"eicr04152020.xml", @"eicr04152020-expected.json", "validation", "15" },

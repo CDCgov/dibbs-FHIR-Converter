0d0666e94c25be250a1c4479e7d2d1f4c47b2e24
Gordon Farrell <gordon@skylight.digital>
2025-11-19 11:41:46 -0700

    test update

--- src/Microsoft.Health.Fhir.Liquid.Converter.UnitTests/Templates/eCR/Resource/Procedure.cs
+++ src/Microsoft.Health.Fhir.Liquid.Converter.UnitTests/Templates/eCR/Resource/Procedure.cs
@@ -104,8 +104,8 @@ namespace Microsoft.Health.Fhir.Liquid.Converter.UnitTests
             Assert.NotNull(actualFhir.Code);
             Assert.NotNull(actualFhir.Performed);
             Assert.NotEmpty(actualFhir.BodySite);
-            Assert.Equal("Why not", actualFhir.ReasonCode.First().Coding.First().Code);
-            Assert.Equal("Couldn't hurt", actualFhir.ReasonCode.Last().Coding.First().Code);
+            Assert.Equal("Why not", actualFhir.ReasonCode.First().text);
+            Assert.Equal("Couldn't hurt", actualFhir.ReasonCode.Last().text);
             Assert.Equal(
                 "METHOD", 
                 actualFhir.GetExtensionValue<CodeableConcept>("http://hl7.org/fhir/StructureDefinition/procedure-method").Coding.First().Code
